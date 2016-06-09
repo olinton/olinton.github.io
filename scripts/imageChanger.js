7dc7ecd075daa620fe267fdc50fc2cf27f8f9784
owenl0 <owenlinton@hotmail.com>
2016-06-09 00:02:58 -0400

    fade

--- scripts/imageChanger.js
+++ scripts/imageChanger.js
@@ -7,9 +7,8 @@ function changeImage()
     if(x >= images.length){
         x = 0;
     } 
-
-    fadeImg(img, 100, true);
-    setTimeout("changeImage()", 5000);
+    setTimeout("changeImage()", fadeTime);    
+    setTimeout(function(){fadeImg(img, 100, true);}, fadeTime - 1000);
 }
 
 function fadeImg(el, val, fade){
@@ -23,12 +22,16 @@ function fadeImg(el, val, fade){
         el.style.opacity = val / 100;
         setTimeout(function(){fadeImg(el, val, fade);}, 10);
     }
+    else{
+        el.style.opacity = 1.0;
+    }
 }
 
 var images = [],
+fadeTime = 5000,
 x = 0;
 
 images[0] = "/images/image_1.jpg";
 images[1] = "/images/image_2.jpg";
 images[2] = "/images/image_3.jpg";
-setTimeout("changeImage()", 5000);
+setTimeout("changeImage()", fadeTime);
