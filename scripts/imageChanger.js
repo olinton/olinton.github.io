32b171ec83f08dbeddb0113602ac024a6f98f322
owenl0 <owenlinton@hotmail.com>
2016-06-09 00:09:55 -0400

    fade complete

--- scripts/imageChanger.js
+++ scripts/imageChanger.js
@@ -2,36 +2,25 @@ function changeImage()
 {
     var img = document.getElementById("bannerImg");
     img.src = images[x];
-    x++;
-
-    if(x >= images.length){
-        x = 0;
-    } 
+    x = (x + 1) % images.length;
     setTimeout("changeImage()", fadeTime);    
-    setTimeout(function(){fadeImg(img, 100, true);}, fadeTime - 1000);
+    setTimeout(function(){fadeImg(img, 100, true);}, fadeTime - 500);
+    fadeImg(img, 0, false);
 }
 
 function fadeImg(el, val, fade){
-    if(fade === true){
-        val--;
-    }else{
-        val ++;
-    }
-
+    val = val + (fade ? -1 : 1);
     if(val > 0 && val < 100){
         el.style.opacity = val / 100;
-        setTimeout(function(){fadeImg(el, val, fade);}, 10);
+        setTimeout(function(){fadeImg(el, val, fade);}, 5);
     }
     else{
         el.style.opacity = 1.0;
     }
 }
 
-var images = [],
+var images = ["images/image_1.jpg", "images/image_2.jpg", "images/image_3.jpg", 
+"images/image_4.jpg", "images/image_5.jpg", "images/image_6.jpg", ],
 fadeTime = 5000,
 x = 0;
-
-images[0] = "/images/image_1.jpg";
-images[1] = "/images/image_2.jpg";
-images[2] = "/images/image_3.jpg";
 setTimeout("changeImage()", fadeTime);
