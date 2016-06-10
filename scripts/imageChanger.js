1150a0cc3e2e9e269d8438908a142ffa06deb6cb
owenl0 <owenlinton@hotmail.com>
2016-06-09 21:32:03 -0400

    tes

--- scripts/imageChanger.js
+++ scripts/imageChanger.js
@@ -19,8 +19,8 @@ function doChangeImage(){
 	setTimeout("changeImage()", fadeTime);
 	setTimeout(function(){fadeImg(img, 30, true);}, fadeTime - 300);
 }
-var images = ["images/image_1.jpg", "images/image_2.jpg", "images/image_3.jpg", 
-"images/image_4.jpg", "images/image_5.jpg", "images/image_6.jpg"],
+var images = ["/images/image_1.jpg", "/images/image_2.jpg", "/images/image_3.jpg", 
+"/images/image_4.jpg", "/images/image_5.jpg", "/images/image_6.jpg"],
 fadeTime = 30000,
 x = 1;
 setTimeout("doChangeImage()", 10);
