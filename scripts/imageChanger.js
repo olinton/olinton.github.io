ca0dae5e39dd86991e63486336d81bdcc08263d9
owenl0 <owenlinton@hotmail.com>
2016-06-09 21:58:35 -0400

    test

--- scripts/imageChanger.js
+++ scripts/imageChanger.js
@@ -9,7 +9,10 @@ function changeImage()
 function fadeImg(el, val, fade){
     val = val + (fade ? -1 : 1);
     if(val > 0 && val < 30){
-        el.style.opacity = val / 30;
+        if(val < 2)
+			el.style.opacity = 0;
+		else
+			el.style.opacity = val / 30;
         setTimeout(function(){fadeImg(el, val, fade);}, 10);
     }
 }
