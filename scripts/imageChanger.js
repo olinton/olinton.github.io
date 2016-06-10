21f5ba3fa425bbaefee64044baa49749d8a0a603
owenl0 <owenlinton@hotmail.com>
2016-06-09 21:05:18 -0400

    fade proper

--- scripts/imageChanger.js
+++ scripts/imageChanger.js
@@ -8,8 +8,8 @@ function changeImage()
 
 function fadeImg(el, val, fade){
     val = val + (fade ? -1 : 1);
-    if(val > 0 && val < 100){
-        el.style.opacity = val / 100;
+    if(val > 0 && val < 30){
+        el.style.opacity = val / 30;
         setTimeout(function(){fadeImg(el, val, fade);}, 10);
     }
 }
@@ -17,10 +17,10 @@ function doChangeImage(){
     var img = document.getElementById("bannerImg");
 	fadeImg(img, 0, false);
 	setTimeout("changeImage()", fadeTime);
-	setTimeout(function(){fadeImg(img, 100, true);}, fadeTime - 1000);
+	setTimeout(function(){fadeImg(img, 30, true);}, fadeTime - 300);
 }
 var images = ["images/image_1.jpg", "images/image_2.jpg", "images/image_3.jpg", 
 "images/image_4.jpg", "images/image_5.jpg", "images/image_6.jpg"],
-fadeTime = 10000,
+fadeTime = 30000,
 x = 1;
-setTimeout("doChangeImage()", 100);
+setTimeout("doChangeImage()", 10);
